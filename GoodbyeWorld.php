abd9860529e92b845dedae195ea9b5d4a44deecf
Stefaniya Shumkova <shumkova@kodeks.ru>
2022-10-05 18:59:34 +0300

    тест

---
+++ GoodbyeWorld.php
@@ -0,0 +1,9 @@
+<?php
+
+class GoodbyeWorld
+{
+    public function sayGoodbye()
+    {
+        echo 'Goodbye World!';
+    }
+}
---
+++ HelloWorld.php
@@ -0,0 +1,9 @@
+<?php
+
+class HelloWorld
+{
+    public function sayHello()
+    {
+        echo 'Hello World!';
+    }
+}
